6a343cf1d654a225f2f7309a4e82c3e9fac1c502
manymengofishing <mattheyj@googlemail.com>
2013-10-31 09:29:51 -0400

    new homepage layout

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
   var $header = $('.header');
 
   $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    console.log(scrollTop);
-    console.log(!$header.hasClass('hidden'));
-    if (scrollTop > 100){
-      $header.addClass('hidden');
-    }
-    else{
-      $header.removeClass('hidden');
-    }
+    // var scrollTop = $(window).scrollTop();
+    // console.log(scrollTop);
+    // console.log(!$header.hasClass('hidden'));
+    // if (scrollTop > 100){
+    //   $header.addClass('hidden');
+    // }
+    // else{
+    //   $header.removeClass('hidden');
+    // }
   });
-  
+
 });
